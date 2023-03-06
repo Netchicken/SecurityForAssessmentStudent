50eed0a66878891a5139e5369a869f08f966e88e
Gary <Gary.Dix@enform.nz>
2023-03-07 12:04:00 +1300

    Added access denied image

--- Pages/Privacy.cshtml.cs
+++ Pages/Privacy.cshtml.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SecurityForAssessmentStudent.Pages
 {
+    [Authorize( Roles = "Admin")]
     public class PrivacyModel : PageModel
     {
         private readonly ILogger<PrivacyModel> _logger;
