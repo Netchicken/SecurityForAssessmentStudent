ae86b61198df7810cb4d5c98495a7914119c0c25
Gary <Gary.Dix@enform.nz>
2023-03-07 11:41:26 +1300

    Add project files.

---
+++ DTO/UserRoles.cs
@@ -0,0 +1,9 @@
+﻿namespace SecurityForAssessmentStudent.DTO
+{
+    public class UserRoles
+    {
+        public string UserName { get; set; }
+        public string RoleName { get; set; }
+
+    }
+}
